b0c6e33f7c972715513aefe7d5ce0de3dfe6cf47
ljohansson <l.johansson@umcg.nl>
2019-05-17 21:10:51 +0200

    updated chapter 9

---
+++ clean_chapter9.sh
@@ -0,0 +1,12 @@
+rm thesis_chapter9.aux
+rm thesis_chapter9.dvi
+rm thesis_chapter9.lot
+rm thesis_chapter9.ps
+rm thesis_chapter9.bbl
+rm thesis_chapter9.lof
+rm thesis_chapter9.out
+rm thesis_chapter9.blg
+rm thesis_chapter9.log
+rm thesis_chapter9.pdf
+rm thesis_chapter9.toc
+
---
+++ run_Lennart_chapter9.sh
@@ -0,0 +1,6 @@
+latex thesis_chapter9.tex
+bibtex thesis_chapter9
+latex thesis_chapter9.tex
+latex thesis_chapter9.tex
+dvips -t a5 thesis_chapter9.dvi
+ps2pdf thesis_chapter9.ps
